b8a2491bf860b65a202bcb5acfde99a8084adcc1
waplord3 <mercyene00@gmail.com>
2025-02-06 23:52:52 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "★wapl★d★",
+  botOwner: '‡wapmon‡', // Your name
+  ownerNumber: '2347047209536', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
